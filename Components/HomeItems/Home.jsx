b55f802980fbb2964298efe4e4ffb1c612c404ad
bathulapraka <bathulaprakash406@gmail.com>
2024-05-29 16:29:43 +0530

    Delete Mail

--- Components/HomeItems/Home.jsx
+++ Components/HomeItems/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import "../HomeItems/Home.css";
 import { useNavigate } from "react-router-dom";
 import ReadMessage from "../ReadMessage/Readmessage";
+import { MdDeleteForever } from "react-icons/md";
 export default function Home() {
   const [emails, setemails] = useState([]);
   const navigate = useNavigate();
@@ -51,6 +52,21 @@ export default function Home() {
         console.error(err);
       });
   };
+
+  const handledelete = (readid) => {
+    axios
+      .delete(
+        `https://mail--box-client-default-rtdb.firebaseio.com/Cart/${readid}.json`
+      )
+      .then(() => {
+        setemails((prevEmails) =>
+          prevEmails.filter((email) => email.id !== readid)
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <div className="All-items">
       <ul>
@@ -67,6 +83,9 @@ export default function Home() {
 
             {/* <li>{email.to}</li> */}
             <li>{email.timestamp}</li>
+            <li  >
+           <button className="del"onClick={() => handledelete(email.id)}>Delete</button> 
+            </li>
           </div>
         ))}
       </ul>
